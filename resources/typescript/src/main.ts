221795002cf7366d2266247ca49035e73d1c7a82
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-08-18 22:51:57 -0500

    tailwind works

--- resources/typescript/src/main.ts
+++ resources/typescript/src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+
 import { setupCounter } from './counter.ts'
+import '../../style/index.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript + Bradley</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+<div>
+	<p class='font-bold text-[50px]'>
+		Hello, World!
+	</p>
+
+		<button id="counter" type="button"></button>
+
+
+</div>
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
